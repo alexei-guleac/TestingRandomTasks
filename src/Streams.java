9882fef774caf4f05b8564f8fb67dbe46f595aa7
aleks.g <al.guleac@gmail.com>
2019-07-10 19:29:49 +0300

    updated files

--- src/Streams.java
+++ src/Streams.java
@@ -1,3 +1,57 @@
-public class Streams {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
+public class Streams {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("John", "Daenerys", "Tyrion", "", null, "Arya");
+
+        names.stream()
+                .filter(Objects::nonNull)
+                .filter(name -> !name.isEmpty() && name.contains("a"))
+                .forEach(System.out::println);
+
+        List<String> stringCollection = new ArrayList<>();
+        stringCollection.add("ddd2");
+        stringCollection.add("aaa2");
+        stringCollection.add("bbb1");
+        stringCollection.add("aaa1");
+        stringCollection.add("bbb3");
+        stringCollection.add("ccc");
+        stringCollection.add("bbb2");
+        stringCollection.add("ddd1");
+
+        stringCollection
+                .stream()
+                .filter((s) -> s.startsWith("a"))
+                .forEach(System.out::println);
+
+        stringCollection
+                .stream()
+                .map(String::toUpperCase)
+                .sorted((a, b) -> b.compareTo(a))
+                .forEach(System.out::println);
+
+        boolean anyStartsWithA =
+                stringCollection
+                        .stream()
+                        .anyMatch((s) -> s.startsWith("a"));
+
+        System.out.println(anyStartsWithA);      // true
+
+        boolean allStartsWithA =
+                stringCollection
+                        .stream()
+                        .allMatch((s) -> s.startsWith("a"));
+
+        System.out.println(allStartsWithA);      // false
+
+        boolean noneStartsWithZ =
+                stringCollection
+                        .stream()
+                        .noneMatch((s) -> s.startsWith("z"));
+
+        System.out.println(noneStartsWithZ);     // true
+    }
 }
